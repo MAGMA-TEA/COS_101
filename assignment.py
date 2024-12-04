6868232964daf831ad26922a7e2747c7c8a92094
Bishop@14 <onuajibili@gmail.com>
2024-12-04 22:51:57 +0100

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,8 +1,5 @@
 number_list = [1, 2, 3, 34 ,45, 5, 4, 4, 78,15]
 
-for number in number_list[::-1]:
-     print(number)
+for numbers in number_list[::-1]:
+     print(numbers)
 
-food_list = ['maggie', 'fish', 'tomatoe', 'indomie']
-print(food_list)
-for
