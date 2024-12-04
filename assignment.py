34d1c5ffb8b8b48de6c3456273e155bfb12deaa6
Bishop@14 <onuajibili@gmail.com>
2024-12-04 22:48:18 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,8 @@
+number_list = [1, 2, 3, 34 ,45, 5, 4, 4, 78,15]
+
+for number in number_list[::-1]:
+     print(number)
+
+food_list = ['maggie', 'fish', 'tomatoe', 'indomie']
+print(food_list)
+for
